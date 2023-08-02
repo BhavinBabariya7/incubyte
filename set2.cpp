bd3d689e77bcd1b12684b5fe5124da54d3964c5a
BhavinBabariya7 <132831715+BhavinBabariya7@users.noreply.github.com>
2023-08-02 14:24:22 +0530

    Add files via upload

---
+++ set2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void calculateCoordinate(char arr[],int &coOrdinates[]) {
+    for(char a : arr ) {
+        char ch;
+        switch(ch) {
+            case 'f':
+                break;
+            case 'b':
+                break;
+            case 'l':
+                break;
+            case 'r':
+                break;
+            case 'u':
+                break;
+            case 'd':
+                break;
+        }
+
+    }
+}
+
+int main() {
+    int coOrdinates[] = {0,0,0}; 
+    char arr[] = {'f','r','u','b','l'};
+    calculateCoordinate(arr,coOrdinates);
+
+    //print answer
+
+    return 0;
+}
