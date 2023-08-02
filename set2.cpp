85a50cc9688a2ded761fcdab3c470b3bbe77b7e1
BhavinBabariya7 <132831715+BhavinBabariya7@users.noreply.github.com>
2023-08-02 15:14:44 +0530

    ready for test cases

--- set2.cpp
+++ set2.cpp
@@ -1,164 +1,143 @@
 #include<iostream>
-#include<bits/stdc++.h>
 using namespace std;
 
-void moveForward(char arr[],int &coOrdinates[],char direction) {
-    switch(direction) {
-            case 'N':
-                
-                break;
-            case 'S':
-                break;
-            case 'E':
-                break;
-            case 'W':
-                break;
-            case 'U':
-                break;
-            case 'D':
-                break;
-        }
-}
-void moveUp(char arr[],int &coOrdinates[],char direction) {
-    switch(direction) {
-            case 'N':
-                
-                break;
-            case 'S':
-                break;
-            case 'E':
-                break;
-            case 'W':
-                break;
-            case 'U':
-                break;
-            case 'D':
-                break;
-        }
-}
-void moveRight(char arr[],int &coOrdinates[],char direction) {
-    switch(direction) {
-            case 'N':
-                direction = 'E';
-                break;
-            case 'S':
-                direction = 'E';
-                break;
-            case 'E':
-                direction = 'N';
-                break;
-            case 'W':
-                direction = 'N';
-                break;
-            case 'U':
-                direction = 'E';
-                break;
-            case 'D':
-                direction = 'E';
-                break;
-        }
-}
-void moveLeft(char arr[],int &coOrdinates[],char direction) {
-    switch(direction) {
-            case 'N':
-                direction = 'W';
-                break;
-            case 'S':
-                direction = 'W';
-                break;
-            case 'E':
-                direction = 'S';
-                break;
-            case 'W':
-                direction = 'S';
-                break;
-            case 'U':
-                direction = 'W';
-                break;
-            case 'D':
-                direction = 'W';
-                break;
-        }
-}
-void moveBackward(char arr[],int &coOrdinates[],char direction) {
-    switch(direction) {
-            case 'N':
-                arr[1]--;
-                break;
-            case 'S':
-                arr[1]--;
-                break;
-            case 'E':
-                arr[1]--;
-                break;
-            case 'W':
-                arr[1]--;
-                break;
-            case 'U':
-                arr[2]--;
-                break;
-            case 'D':
-                arr[2]--;
-                break;
-        }
+void moveDown(char arr[], int coOrdinates[], char &direction) {
+    direction = 'D';
 }
 
-void moveForward(char arr[],int &coOrdinates[],char direction) {
-    switch(direction) {
-            case 'N':
-                arr[1]++;
-                break;
-            case 'S':
-                arr[1]++;
-                break;
-            case 'E':
-                arr[1]++;
-                break;
-            case 'W':
-                arr[1]++;
-                break;
-            case 'U':
-                arr[2]++;
-                break;
-            case 'D':
-                arr[2]++;
-                break;
-        }
+void moveUp(char arr[], int coOrdinates[], char &direction) {
+    direction = 'U';
 }
 
-void calculateCoordinate(char arr[],int &coOrdinates[],char direction) {
-    for(char a : arr ) {
-        char ch;
-        switch(ch) {
-            case 'f':
-                moveForward(arr,coOrdinates,direction);
-                break;
-            case 'b':
-                moveBackward(arr,coOrdinates,direction);
-                break;
-            case 'l':
-                moveLeft(arr,coOrdinates,direction);
-                break;
-            case 'r':
-                moveRight(arr,coOrdinates,direction);
-                break;
-            case 'u':
-                moveUp(arr,coOrdinates,direction);
-                break;
-            case 'd':
-                break;
-        }
+void moveRight(char arr[], int coOrdinates[], char &direction) {
+    switch (direction) {
+    case 'N':
+        direction = 'E';
+        break;
+    case 'S':
+        direction = 'W';
+        break;
+    case 'E':
+        direction = 'S';
+        break;
+    case 'W':
+        direction = 'N';
+        break;
+    case 'U':
+        direction = 'E';
+        break;
+    case 'D':
+        direction = 'E';
+        break;
+    }
+}
 
+void moveLeft(char arr[], int coOrdinates[], char &direction) {
+    switch (direction) {
+    case 'N':
+        direction = 'W';
+        break;
+    case 'S':
+        direction = 'E';
+        break;
+    case 'E':
+        direction = 'N';
+        break;
+    case 'W':
+        direction = 'S';
+        break;
+    case 'U':
+        direction = 'W';
+        break;
+    case 'D':
+        direction = 'W';
+        break;
+    }
+}
+
+void moveBackward(char arr[], int coOrdinates[], char direction) {
+    switch (direction) {
+    case 'N':
+        coOrdinates[1]--;
+        break;
+    case 'S':
+        coOrdinates[1]++;
+        break;
+    case 'E':
+        coOrdinates[0]++;
+        break;
+    case 'W':
+        coOrdinates[0]--;
+        break;
+    case 'U':
+        coOrdinates[2]--;
+        break;
+    case 'D':
+        coOrdinates[2]++;
+        break;
+    }
+}
+
+void moveForward(char arr[], int coOrdinates[], char direction) {
+    switch (direction) {
+    case 'N':
+        coOrdinates[1]++;
+        break;
+    case 'S':
+        coOrdinates[1]--;
+        break;
+    case 'E':
+        coOrdinates[0]--;
+        break;
+    case 'W':
+        coOrdinates[0]++;
+        break;
+    case 'U':
+        coOrdinates[2]++;
+        break;
+    case 'D':
+        coOrdinates[2]--;
+        break;
+    }
+}
+
+void calculateCoordinate(char arr[], int coOrdinates[], char &direction) {
+    for (int i = 0; i < 5; i++) {
+        char ch = arr[i];
+        switch (ch) {
+        case 'f':
+            moveForward(arr, coOrdinates, direction);
+            break;
+        case 'b':
+            moveBackward(arr, coOrdinates, direction);
+            break;
+        case 'l':
+            moveLeft(arr, coOrdinates, direction);
+            break;
+        case 'r':
+            moveRight(arr, coOrdinates, direction);
+            break;
+        case 'u':
+            moveUp(arr, coOrdinates, direction);
+            break;
+        case 'd':
+            moveDown(arr, coOrdinates, direction);
+            break;
+        }
     }
 }
 
 int main() {
-    int coOrdinates[] = {0,0,0}; 
-    char direction = "N";
-    char arr[] = {'f','b'};
-    calculateCoordinate(arr,coOrdinates,direction);
+    int coOrdinates[] = { 0, 0, 0 };
+    char direction = 'N';
+    char arr[] = { 'f', 'r', 'u', 'b', 'l' };
+    calculateCoordinate(arr, coOrdinates, direction);
 
     //print answer
-
+    cout << "Co-ordinates : ";
+    for (int i = 0; i < 3; i++) {
+        cout << " " << coOrdinates[i];
+    }
+    cout << " Direction : " << direction << endl;
     return 0;
 }
